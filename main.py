60dcf054ea2db3c91fd65b99bae6d19804dd0e1c
Arpan Ghosh <arpanghosh95@gmail.com>
2023-08-03 14:08:31 -0400

    functionalize

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import dbutils
 from PIL import Image
 import pytesseract
 import openai
+import pprint
 
 openai.organization = "org-87S7CpTnHb0kAdCaM9m5NorN"
 OPENAI_API_KEY = "INSERT_API_KEY_HERE"
@@ -10,28 +11,47 @@ openai.Model.list()
 
 uploaded_file = "/FileStore/tables/question_screenshot_example.PNG"
 
+def debug_print(value):
+    pprint.PrettyPrinter().pprint(value)
+
+def get_image():
+    image_file_location = "/tmp/question_screenshot_example"
+    return image_file_location
+
 #Copy from Databricks Community Edition DBFS to accessible /tmp directory
-dbutils.fs.cp(uploaded_file, "file:///tmp/question_screenshot_example")
+def copy_sourcefile():
+    dbutils.fs.cp(uploaded_file, "file:///tmp/question_screenshot_example")
 
-# File location and type
-file_location = "/tmp/question_screenshot_example"
-raw_text = pytesseract.image_to_string(Image.open(file_location))
-chunks = raw_text.split('\n')
-print(raw_text)
+def ocr_to_text(image):
+    raw_text = pytesseract.image_to_string(Image.open(file_location))
+    chunks = raw_text.split('\n')
+    debug_print(raw_text)
+    return chunks
 
-question = chunks[0]
-question = question.lstrip('0123456789.- ')
-print(question)
+def clean_question(question):
+    return question.lstrip('0123456789.- ')
 
-response = openai.Completion.create(
-    model="gpt-3.5-turbo",
-    prompt=question,
-    temperature=0,
-    max_tokens=100,
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=["\n"]
-)
+def parse_question(chunks):
+    question = clean_question(chunks[0])
+    debug_print(question)
+    return question
 
-print(response)
+def ask_chatgpt(question):
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt=question,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+
+    debug_print(response)
+
+if __name__ == "__main__":
+    copy_sourcefile()
+    data = ocr_to_text(get_image())
+    question = parse_question(chunks)
+    ask_chatgpt(question)
